2f9b458c2e79ac48d0a83055d4d853ea36f7bd24
mbed101 <mahend101@protonmail.com>
2023-07-09 18:59:46 +0200

    added filesystems

---
+++ Filesystems/RenameFilesWithExtensions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Directory where the files are located
+directory="/path/to/directory"
+
+# Extension to search for
+extension=".txt"
+
+# New prefix for renamed files
+new_prefix="new_"
+
+# Loop through the files in the directory
+for file in "$directory"/*"$extension"; do
+  if [ -f "$file" ]; then
+    # Get the file name without the directory path
+    filename=$(basename "$file")
+
+    # Generate the new file name with the new prefix
+    new_filename="$new_prefix$filename"
+
+    # Rename the file
+    mv "$file" "$directory/$new_filename"
+
+    echo "Renamed $filename to $new_filename"
+  fi
+done
